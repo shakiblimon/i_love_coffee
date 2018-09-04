cb9916068ac9500965ab73d93fe9e1dea0f0520c
S . M. Shakib Limon <{shakiblimon@gmx.com}+{limon}@users.noreply.github.com>
2018-09-05 02:35:18 +0600

    Deploy Heroku App

--- i_love_coffee/wsgi.py
+++ i_love_coffee/wsgi.py
@@ -11,7 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'i_love_coffee.settings')
 
 application = get_wsgi_application()
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
